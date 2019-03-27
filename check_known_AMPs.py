882f208768423e2b4052bd6e692ed993ec1590d2
gcheng314 <48725587+gcheng314@users.noreply.github.com>
2019-03-27 09:47:36 -0400

    V1.0

--- check_known_AMPs.py
+++ check_known_AMPs.py
@@ -1,32 +1,46 @@
 def knowns(infile):
+    '''
+    knowns reads in a fasta file, filters the queries by true positive AMPs based on the Uniprot format, 
+    and writes the filtered queries to a new fasta file. Additionally, the number of true positives     
+    
+    Requires:
+    file is downloaded from Uniprot
+    
+    Example:
+    
+    If knowns is run with the proteome of o. mykiss as its parameter, the output file contains 
+    all queries from the proteome and the printed value is 8
+    '''
+    
     list_amps = ['Bacteriocin', 'Brevinin', 'Beta-defensin', 'Cathelicidin', 
                  'Defensin', 'Dermaseptin', 'Latarcin', 'Palustrin', 'Rugosin', 
-                 'Cecropin', 'LEAP', 'LEAP2', 'Liver-expressed', 'Penaeidin', 
-                 'Tachystatin', 'Esculentin', 'Maximin', 'Phylloseptin', 
+                 'Cecropin', 'LEAP', 'LEAP2', 'Liver-expressed', 'liver-expressed', 
+                 'Penaeidin', 'Tachystatin', 'Esculentin', 'Maximin', 'Phylloseptin', 
                  'Temporin', 'Pleurocidin', 'Thaumatin', 'Formaecin', 
                  'Melittin', 'Ponericin', 'Thionin', 'Ascaphin', 'Caerin', 
                  'Clavanin', 'Gaegurin', 'Metchnikowin', 'Pseudin', 'Aurein', 
-                 'Bombinin', 'Coleoptericin', 'Hepcidin', 'Moricin', 
+                 'Bombinin', 'Coleoptericin', 'Hepcidin', 'Hepcidin-1', 'Moricin', 
                  'Ranateurin', 'Transferrin', 'Cupiennin', 'Histatin',
                  'Myticin', 'Uperin']
-    q = 0
-    f = open(infile, 'r')
-    o = open(infile[:infile.index('.')]+'_TP.txt', 'w')
-    line = f.readline()
+    num_queries = 0
+    file_input = open(infile, 'r')
+    file_output = open(infile[:infile.index('.')]+'_TP.txt', 'w')
+    line = file_input.readline()
     
     while line != '':
+       
         if line.startswith('>') and line.split(' ')[1] in list_amps:
-            q += 1
-            acc = line
-            o.write(acc)
-            line = f.readline()
+            num_queries += 1
+            accession = line
+            file_output.write(accession)
+            line = file_input.readline()
             
             while not(line.startswith('>')):
-                o.write(line)
-                line = f.readline()
+                file_output.write(line)
+                line = file_input.readline()
                            
         else:
-            line = f.readline()
-    
-    f.close()   
-    o.close()
+            line = file_input.readline()
+    print(str(num_queries) + ' known AMPs are detected')
+    file_input.close()   
+    file_output.close()
